20ec4ee749fc194d6398ff83f8d6a2459f8f8b78
jamers7237 <jamesli3763@gmail.com>
2024-11-12 01:19:45 +0000

    Pushing my boba site!

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function clickButton() {
-  console.log("hi");
-  let button = document.getElementsByTagName("button")[0];
-  button.textContent =
-    "Clicks: " + (parseInt(button.textContent.split(" ")[1]) + 1);
+function updateCountdown() {
+  const christmasDate = new Date('2024-12-25T00:00:00').getTime();
+  const currentTime = new Date().getTime();
+  const timeLeft = christmasDate - currentTime;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('seconds').innerHTML = seconds;
+
+  const isChristmasDay = new Date().toDateString() === new Date('2024-12-25').toDateString();
+
+  if (isChristmasDay) {
+      document.getElementById('message').innerHTML = "Merry Christmas!";
+      clearInterval(interval);
+  } else {
+      document.getElementById('message').innerHTML = "It's not Christmas yet :(";
+  }
 }
+
+const interval = setInterval(updateCountdown, 1000);
+
+updateCountdown();
